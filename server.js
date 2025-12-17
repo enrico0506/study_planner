87e0c85340e3e99cfc67bfff2a5ed6de71d21deb
Enrico Fachin <enrico.fachin@hm.edu>
2025-12-17 12:31:48 +0100

    debug

--- server.js
+++ server.js
@@ -139,6 +139,7 @@ app.get("/calendar.html", (_req, res) => sendHtml(res, "calendar.html"));
 app.get("/stundenplan.html", (_req, res) => sendHtml(res, "stundenplan.html"));
 app.get("/karteikarten.html", (_req, res) => sendHtml(res, "karteikarten.html"));
 app.get("/account.html", (_req, res) => sendHtml(res, "account.html"));
+app.get("/notizen.html", (_req, res) => sendHtml(res, "notizen.html"));
 app.get("/study-confidence-table.html", (_req, res) =>
   sendHtml(res, "study-confidence-table.html")
 );
